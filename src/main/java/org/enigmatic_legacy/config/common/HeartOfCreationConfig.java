7da4879d5976e7667b0cf98a42b5aebdd6588f67
ZY2012303834 <0611+ZY2012303834@users.noreply.github.com>
2026-06-25 12:54:20 +0800

    Update

--- src/main/java/org/enigmatic_legacy/config/common/HeartOfCreationConfig.java
+++ src/main/java/org/enigmatic_legacy/config/common/HeartOfCreationConfig.java
@@ -17,15 +17,15 @@ public class HeartOfCreationConfig {
 
         cooldown = builder
                 .comment("创造之心主动能力冷却时间，单位为刻。20 刻 = 1 秒。默认 60 = 3 秒。")
-                .defineInRange("cooldown", 60, 0, Integer.MAX_VALUE);
+                .defineInRange("cooldown", 20, 0, Integer.MAX_VALUE);
 
         lightningRange = builder
                 .comment("创造之心主动能力影响范围。范围内敌人会被闪电击中并获得凋零效果。")
-                .defineInRange("lightningRange", 16.0D, 1.0D, 128.0D);
+                .defineInRange("lightningRange", 32.0D, 1.0D, 128.0D);
 
         lightningDamage = builder
                 .comment("创造之心主动能力每次闪电造成的伤害。默认 10。")
-                .defineInRange("lightningDamage", 10.0D, 0.0D, 10000.0D);
+                .defineInRange("lightningDamage", 20.0D, 0.0D, 10000.0D);
 
         builder.pop();
     }
